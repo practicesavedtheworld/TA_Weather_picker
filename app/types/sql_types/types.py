80eb833b0471460f534412a0c022bb3bd4bb59b5
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-09-13 16:06:27 +0300

    update sql types

--- app/types/sql_types/types.py
+++ app/types/sql_types/types.py
@@ -1,12 +1,11 @@
 from typing import TypeAlias, Annotated
-import datetime
 
 from sqlalchemy import sql
 from sqlalchemy.orm import DeclarativeBase, mapped_column
-from sqlalchemy.types import FLOAT, DECIMAL, INTEGER
+from sqlalchemy.types import FLOAT, DECIMAL, INTEGER, DATETIME
 
 Base: TypeAlias = DeclarativeBase
-Now: TypeAlias = Annotated[datetime.datetime, mapped_column(default_factory=sql.func.now)]
+Now: TypeAlias = Annotated[DATETIME, mapped_column(default_factory=sql.func.now)]
 Coordinates: TypeAlias = Annotated[FLOAT, mapped_column(nullable=True)]
 SQL_FLOAT: TypeAlias = Annotated[FLOAT | DECIMAL, mapped_column(nullable=True, default=0.)]
 SQL_INT: TypeAlias = Annotated[INTEGER, mapped_column(nullable=True, default=0)]
