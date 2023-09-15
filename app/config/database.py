dc28f0b9ffc715fe4713f9eeb762b1ffe0bee77c
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-09-15 15:14:00 +0300

    add database connection

---
+++ app/config/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.exc import SQLAlchemyError
+
+from .configurations import settings
+from .logger import create_logger
+from app.types import connected
+
+logger = create_logger(
+    logger_name="database_logger",
+    logger_level="DEBUG",
+)
+
+try:
+    async_engine: AsyncEngine = create_async_engine(settings.DB_URL)
+    session: AsyncSession = async_sessionmaker(
+        bind=async_engine,
+        expire_on_commit=False,
+    )  # type ignore
+    logger.info("Create engine. Ready to use")
+    connected = True
+except SQLAlchemyError as db_err:
+    connected = False
+    logger.critical("Won't able connect to database. There is some problem occur", exc_info=db_err)
+finally:
+    logger.debug(f"Connect to database. Status {'success' if connected else 'failed'}")
