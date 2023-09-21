f4162cc627466c3b0e87e9d6e73c88a343dfd0c1
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-09-21 15:03:24 +0300

    update with dev tools

--- app/cities/cities.py
+++ app/cities/cities.py
@@ -1,11 +1,11 @@
 import functools
-from typing import Iterator, Optional, Iterable
 from abc import ABC, abstractmethod
+from typing import Iterable, Iterator, Optional
 
 import geonamescache
 
-from app.exceptions import NoCitiesSelected
 from app.config import create_logger
+from app.exceptions import NoCitiesSelected
 
 logger = create_logger(
     logger_name="cities_logger",
@@ -19,7 +19,9 @@ class WorldCities(ABC):
     def __new__(cls, *args, **kwargs):
         if cls._cities_instance is None:
             cls._cities_instance = super().__new__(cls)
-            cls._cities_instance.world_cities = geonamescache.GeonamesCache().get_cities()
+            cls._cities_instance.world_cities = (
+                geonamescache.GeonamesCache().get_cities()
+            )
         return cls._cities_instance
 
     @staticmethod
@@ -37,30 +39,40 @@ class WorldCities(ABC):
 
 
 class Cities(WorldCities):
+    def get_largest_cities_by_quantity(
+        self, quantity: int = 100, min_population: int = 1_000_000
+    ) -> list[dict]:
+        """Receive the largest cities in the world according to
+        geonamescache library data.
 
-    def get_largest_cities_by_quantity(self, quantity: int = 100, min_population: int = 1_000_000) -> list[dict]:
-        """Receive the largest cities in the world according to geonamescache library data.
-
-        According to the Earth population ~8_000_000_000, min_population param default is 1_000_000.
-        When you need more that top 1000 cities, you may need to decrease the min_population param.
+        According to the Earth population ~8_000_000_000, min_population
+        param default is 1_000_000.
+        When you need more that top 1000 cities, you may need to decrease
+        the min_population param.
         The more cities you get, the less min_population you set"""
         try:
             assert quantity > 0
         except AssertionError:
-            raise NoCitiesSelected(exc_details=f"Your city quantity is {quantity}. Change it")
+            raise NoCitiesSelected(
+                exc_details=f"Your city quantity is {quantity}. Change it"
+            )
         largest_cities: Iterator[dict] = (
-            city for city in self.world_cities.values()
+            city
+            for city in self.world_cities.values()
             if city["population"] >= min_population
         )
         logger.debug(f"Successfully receive {quantity} largest city")
         return self.sort_cities_by_population(largest_cities)[-quantity:]
 
     @functools.lru_cache(maxsize=20)
-    def get_largest_cities_names(self, quantity: int = 100, min_population: int = 1_000_000) -> list[str]:
+    def get_largest_cities_names(
+        self, quantity: int = 100, min_population: int = 1_000_000
+    ) -> list[str]:
         """Getting a list of names of the largest cities"""
 
         return [
-            city["name"] for city in self.get_largest_cities_by_quantity(
+            city["name"]
+            for city in self.get_largest_cities_by_quantity(
                 quantity=quantity,
                 min_population=min_population,
             )
